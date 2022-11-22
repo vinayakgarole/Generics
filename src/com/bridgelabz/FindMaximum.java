437ab22756877b0874c9c6e08266f7b2c7cd6001
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-22 23:22:16 +0530

    Added: Refactor all the 3 to

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -2,33 +2,9 @@ package com.bridgelabz;
 
 public class FindMaximum {
 
-    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
+    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
 
-        Integer max = a1;
-
-        if (a2.compareTo(max) > 0)
-            max = a2;
-
-        if (a3.compareTo(max) > 0)
-            max = a3;
-        return max;
-    }
-
-    public static Float maxOfFloat(Float a1, Float a2, Float a3) {
-
-        Float max = a1;
-
-        if (a2.compareTo(max) > 0)
-            max = a2;
-
-        if (a3.compareTo(max) > 0)
-            max = a3;
-        return max;
-    }
-
-    public static String maxOfString(String a1, String a2, String a3) {
-
-        String max = a1;
+        T max = a1;
 
         if (a2.compareTo(max) > 0)
             max = a2;
@@ -39,12 +15,9 @@ public class FindMaximum {
     }
 
     public static void main(String[] args) {
-
-        System.out.println("Welcome To Find Maximum Problem Using Generics\n");
-        System.out.println("The maximum value between the three integer is: " + maxOfInteger(3, 8, 5));
-        System.out.println("------------------------------------------------");
-        System.out.println("The maximum value between the three float is: " + maxOfFloat(3.12F, 8.14F, 5.32F));
-        System.out.println("------------------------------------------------");
-        System.out.println("The maximum value between the three String is: " + maxOfString("Apple", "Banana", "Peach"));
+        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+        System.out.println("The maximum value between the three integer is: " + maxOfValues(3, 8, 5) + "\n");
+        System.out.println("The maximum value between the three float is: " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
+        System.out.println("The maximum value between the three string is: " + maxOfValues("abc", "pqr", "xyz"));
     }
 }
