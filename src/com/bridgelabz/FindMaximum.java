49d04b7faf3dc612c03c4a7f634fc5cb9804b419
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-22 13:15:55 +0530

    Added: Find the Maximum Integer

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -2,8 +2,21 @@ package com.bridgelabz;
 
 public class FindMaximum {
 
+    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3){
+
+        Integer max = a1;
+
+        if (a2.compareTo(max) > 0)
+            max = a2;
+
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome To Find Maximum Problem Using Generics");
+        System.out.println("The maximum value between the three integer is: " + maxOfInteger(3, 8, 5));
     }
 }
