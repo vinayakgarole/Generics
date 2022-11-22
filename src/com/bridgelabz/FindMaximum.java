82a3e75302ec12f6bf3de065dcd89782ef64daf3
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-22 13:09:41 +0530

    Added: Welcome To Find Maximum Problem Using Generics

---
+++ src/com/bridgelabz/FindMaximum.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class FindMaximum {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Find Maximum Problem Using Generics");
+    }
+}
