f4863527954a05f421959e793f4110de89809882
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-23 13:26:57 +0530

    Added: Refactor to create Generics

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -1,6 +1,15 @@
 package com.bridgelabz;
 
-public class FindMaximum {
+public class FindMaximum<T extends Comparable<T>> {
+
+    T a1, a2, a3;
+
+    public FindMaximum(T a1, T a2, T a3) {
+
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
 
     public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
 
@@ -15,9 +24,10 @@ public class FindMaximum {
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+
+        System.out.println("Welcome To Find Maximum Problem Using Generics \n");
         System.out.println("The maximum value between the three integer is: " + maxOfValues(3, 8, 5) + "\n");
-        System.out.println("The maximum value between the three float is: " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
-        System.out.println("The maximum value between the three string is: " + maxOfValues("abc", "pqr", "xyz"));
+        System.out.println("The maximum value between the three float is: " + maxOfValues(1.5F, 3.56F, 5.87F) + "\n");
+        System.out.println("The maximum value between the three string is: " + maxOfValues("Apple", "Peach", "Banana"));
     }
 }
